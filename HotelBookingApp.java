16028f3bcfad3c1d2e5c970c951698909ddfe730
Vasthavi3 <vd9840@srmist.edu.in>
2026-03-09 14:17:58 +0530

    UC2: Basic Room Types & Static Availability

--- HotelBookingApp.java
+++ HotelBookingApp.java
@@ -2,10 +2,26 @@ public class HotelBookingApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Book My Stay");
-        System.out.println("Hotel Booking Management System");
-        System.out.println("Application Version: 1.0");
-        System.out.println("Application started successfully.");
+        System.out.println("Hotel Booking System v1.0");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("\nRoom Details:");
+
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable);
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable);
+
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
 
     }
 }
---
+++ SingelRomm.java
@@ -0,0 +1,7 @@
+class SingleRoom extends Room {
+
+    SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+
+}
