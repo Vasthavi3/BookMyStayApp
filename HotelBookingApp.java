b82d51b44e5dd7487cb3929b936ed6af38873d7b
Vasthavi3 <vd9840@srmist.edu.in>
2026-03-09 14:27:19 +0530

    UC3: Centralized Room Inventory Management

--- HotelBookingApp.java
+++ HotelBookingApp.java
@@ -1,30 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class HotelBookingApp {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Book My Stay");
-        System.out.println("Hotel Booking System v1.0");
+        System.out.println("Hotel Booking Management System");
 
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
+        RoomInventory inventory = new RoomInventory();
 
-        Map<String, Integer> inventory = new HashMap<>();
-
-        inventory.put("Single Room", 5);
-        inventory.put("Double Room", 3);
-        inventory.put("Suite Room", 2);
-
-        single.displayRoomDetails();
-        System.out.println("Available: " + inventory.get("Single Room"));
-
-        doubleRoom.displayRoomDetails();
-        System.out.println("Available: " + inventory.get("Double Room"));
-
-        suite.displayRoomDetails();
-        System.out.println("Available: " + inventory.get("Suite Room"));
+        System.out.println("\nRoom Inventory:");
+        inventory.displayInventory();
     }
 }
